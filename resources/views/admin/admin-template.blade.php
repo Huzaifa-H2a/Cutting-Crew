fd8575c9e36b05d795beae3de44352c698efc46f
Huzaifa Ahmed <huzaifaahmed851@gmail.com>
2023-06-17 01:20:20 +0500

    Auth (login/signup)

--- resources/views/admin/admin-template.blade.php
+++ resources/views/admin/admin-template.blade.php
@@ -194,7 +194,7 @@
                       <div class="dropdown-divider"></div>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="#">
+                      <a class="dropdown-item" href="/profile">
                         <i class="bx bx-user me-2"></i>
                         <span class="align-middle">My Profile</span>
                       </a>
